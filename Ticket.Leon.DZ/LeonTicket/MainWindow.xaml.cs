32fddf0361b70c5d121074382577bacf26bb2af0
Leon <144522614+Leon228228@users.noreply.github.com>
2025-05-19 12:40:08 +0300

    Add files via upload

---
+++ Ticket.Leon.DZ/LeonTicket/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LeonTicket
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void logon_Click(object sender, RoutedEventArgs e)
+        {
+         if (login.Text == "Leon" && password.Password == "121296")
+            {
+                BuyTicket ticket = new BuyTicket();
+                ticket.Show();
+                this.Close();
+            }
+
+        
+        
+        }
+    }
+}
